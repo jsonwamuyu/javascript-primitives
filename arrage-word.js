2fcd84f0da3309cb018855b4344bf1b484c15b1b
jsonwamuyu <jysonmuchiri@gmail.com>
2025-01-13 03:16:15 -0800

    Sort names

---
+++ arrage-word.js
@@ -0,0 +1,11 @@
+// Question One
+// Arrange the following names in alphabetical order:
+// ["Zara", "Alice", "Michael", "Bob", "Christine"]
+
+function arrangeNames() {
+    let names = ["Zara", "Alice", "Michael", "Bob", "Christine"];
+    let sortedNames = names.sort();
+    return sortedNames
+}
+
+console.log(arrangeWord(["Zara", "Alice", "Michael", "Bob", "Christine"])); // Output: ["Alice", "Bob", "Christine", "Michael", "Zara"]
