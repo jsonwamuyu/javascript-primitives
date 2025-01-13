813053b6bdbb532ab277c19a6a586a6090ea7a88
jsonwamuyu <jysonmuchiri@gmail.com>
2025-01-13 03:52:27 -0800

    Refactor

--- reverse-string.js
+++ reverse-string.js
@@ -2,19 +2,8 @@
 
 // Write a function that reverses a string
 
-
-// Function to reverse a string
-function reverseString(str) {
-    //trim any white space
-    trimStr = str.trim();
-    // now split the string into an array
-    var splitString = trimStr.split("");
-    // reverse the array
-    let reversedArray = splitString.revese()
-    // join the array back to a string
-    let reversedString = reversedArray.join("");
-
-    return reversedString;
+function reverseString(myString) {
+    return myString.split("").reverse().join("");
 }
 
-console.log(reverseString("Hello World!")); // Output: "!dlroW olleH"
+console.log(reverseString("Mike"));
